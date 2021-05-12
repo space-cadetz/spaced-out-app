dc7278917b1bc2640670627ef294133b607d7ea9
Marie <marie.marcos25@gmail.com>
2021-05-11 21:30:36 -0700

    feat: starter apod request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      fetchedData: false
+    };
+  }
+
+  // Nasa APOD query example:
+  // https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+  // https://api.nasa.gov/
+
+  fetchData = async () => {
+    console.log('fetching data');
+    let apodData = await axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
+    console.log('apod data:', apodData);
+    let pod = apodData.data;
+    console.log('pic of the day:', pod);
+    this.setState({
+      title: apodData.data.title,
+      fetchedData: true
+    });
+  }
+
+  render() {
+    return (<>
+      <h1 onClick={this.fetchData}>NASA APOD!</h1>
+      { this.state.fetchedData ?
+        (<>
+          <h2>{this.state.title}</h2>
+        </>)
+        : ''}
+
+    </>
+    );
+  }
 }
+
 
 export default App;
